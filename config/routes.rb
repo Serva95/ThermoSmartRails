b8bf6e1f6f14ade3fde2226c0bff77858df36f1b
Serva <43950391+Serva95@users.noreply.github.com>
2020-05-27 01:29:06 +0200

    finish home + logi/register

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
-  root to: 'application#index'
+  root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "/404", :to => "errors#not_found"
