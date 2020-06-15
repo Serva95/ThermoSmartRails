454c64accae588f9af8c4a4101daad4c47dc6311
Serva <43950391+Serva95@users.noreply.github.com>
2020-06-16 01:09:18 +0200

    added orari_on_offs parsing method

--- app/controllers/orari_on_offs_controller.rb
+++ app/controllers/orari_on_offs_controller.rb
@@ -1,91 +1,53 @@
 class OrariOnOffsController < ApplicationController
-      before_action :set_orari, only: [:edit, :update]
+  before_action :set_orari, only: [:edit, :update]
 
-    def index
-      @orari = OrariOnOff.all
-    end
+  def index
+    @orari = OrariOnOff.all
+  end
 
-    def new
-      @orario = OrariOnOff.new
-      @days = %w[Lunedì Martedì Mercoledì Giovedì Venerdì Sabato Domenica]
-    end
+  def new
+    @orario = OrariOnOff.new
+    @days = %w[Lunedì Martedì Mercoledì Giovedì Venerdì Sabato Domenica]
+  end
 
-    # POST /rooms
-    def create
-      @orario = OrariOnOff.new(orari_params)
-      actual = 0
-      previous_value = 0
-      params.each do |key, value|
-        if !value.empty? && key[0..3] == "time"
-          case key[-1, 1].to_i
-            #per ora controlla solo che siano in ordine temporale
-          when 0
-            #creare un meotodo con sta roba?????
-            if previous_value == 0
-              previous_value = value
-            else
-              break if previous_value >= value
-              previous_value = value
-            end
-            #
-            puts "zero #{key} => #{value}"
-          when 1
-            if actual != 1
-              actual = 1
-
-            end
-            puts "uno #{key} => #{value}"
-          when 2
-            puts "2 #{key} => #{value}"
-          when 3
-            puts "3 #{key} => #{value}"
-          when 4
-            puts "4 #{key} => #{value}"
-          when 5
-            puts "5 #{key} => #{value}"
-          when 6
-            puts "6 #{key} => #{value}"
-          else
-          end
-        end
-      end
-      byebug
-      respond_to do |format|
-        if @room.save
-          format.html { redirect_to rooms_path , notice: 'Stanza creata' }
-          format.json { render :index, status: :created, location: @room }
-        else
-          format.html { render :new }
-          format.json { render json: @room.errors, status: :unprocessable_entity }
+  # POST /rooms
+  def create
+    @orario = OrariOnOff.parse(params)
+    respond_to do |format|
+      if @orario.nil?
+        format.html { redirect_to new_room_orari_on_off_path(params[:room_id]), notice: 'Errore nell\'inserimento degli orari, controlla meglio e riprova'   }
+        format.json { render json: @orario.errors, status: :unprocessable_entity }
+      else
+        #ciclare il salvataggio in una transaction ActiveRecord::Base.transaction do
+        if @orario.save
+          format.html { redirect_to rooms_path , notice: 'Orari salvati' }
+          format.json { render :index, status: :created, location: @orario }
         end
       end
     end
+  end
 
-    #GET /rooms/:id/edit
-    def edit
-    end
+  #GET /rooms/:id/edit
+  def edit
+  end
 
-    # PATCH/PUT /rooms/:id
-    def update
-      respond_to do |format|
-        if @orario.update(room_params)
-          format.html { redirect_to room_orari_on_offs_path, notice: 'Stanza modificata con successo' }
-          format.json { render :index, status: :ok, location: @orario }
-        else
-          format.html { render :edit }
-          format.json { render json: @orario.errors, status: :unprocessable_entity }
-        end
+  # PATCH/PUT /rooms/:id
+  def update
+    respond_to do |format|
+      if @orario.update(room_params)
+        format.html { redirect_to room_orari_on_offs_path, notice: 'Stanza modificata con successo' }
+        format.json { render :index, status: :ok, location: @orario }
+      else
+        format.html { render :edit }
+        format.json { render json: @orario.errors, status: :unprocessable_entity }
       end
     end
+  end
 
-    private
+  private
 
-    def orari_params
-      params.require(:room).permit(:nome, :max_temp, :min_temp, :absolute_min)
-    end
-
-    def set_orario
-      @orario = OrariOnOff.find(params[:id])
-    end
+  def set_orario
+    @orario = OrariOnOff.find(params[:id])
+  end
 
 end
--- app/models/orari_on_off.rb
+++ app/models/orari_on_off.rb
@@ -1,3 +1,179 @@
 class OrariOnOff < ApplicationRecord
   belongs_to :room
+
+  def self.parse(params)
+    orari = []
+    giorno = []
+    actual = 0
+    previous_value = nil
+    params.each do |key, value|
+      if !value.empty? && key[0..3] == "time"
+        case key[-1, 1].to_i
+        when 0
+          #creare un meotodo con sta roba?????
+          if previous_value.nil?
+            previous_value = value
+          else
+            if previous_value >= value
+              orari = nil
+              break
+            end
+            chop = key.dup
+            chop.chop!
+            if chop == "timeoffuno" || chop == "timeoffdue" ||chop == "timeofftre"
+              giorno << previous_value
+              giorno << value
+            end
+            previous_value = nil
+          end
+        when 1
+          if actual != 1
+            orari << giorno
+            giorno = []
+            previous_value = nil
+            actual = 1
+          end
+
+          if previous_value.nil?
+            previous_value = value
+          else
+            if previous_value >= value
+              orari = nil
+              break
+            end
+            chop = key.dup
+            chop.chop!
+            if chop == "timeoffuno" || chop == "timeoffdue" ||chop == "timeofftre"
+              giorno << previous_value
+              giorno << value
+            end
+            previous_value = nil
+          end
+        when 2
+          if actual != 2
+            orari << giorno
+            giorno = []
+            previous_value = nil
+            actual = 2
+          end
+
+          if previous_value.nil?
+            previous_value = value
+          else
+            if previous_value >= value
+              orari = nil
+              break
+            end
+            chop = key.dup
+            chop.chop!
+            if chop == "timeoffuno" || chop == "timeoffdue" ||chop == "timeofftre"
+              giorno << previous_value
+              giorno << value
+            end
+            previous_value = nil
+          end
+        when 3
+          if actual != 3
+            orari << giorno
+            giorno = []
+            previous_value = nil
+            actual = 3
+          end
+
+          if previous_value.nil?
+            previous_value = value
+          else
+            if previous_value >= value
+              orari = nil
+              break
+            end
+            chop = key.dup
+            chop.chop!
+            if chop == "timeoffuno" || chop == "timeoffdue" ||chop == "timeofftre"
+              giorno << previous_value
+              giorno << value
+            end
+            previous_value = nil
+          end
+        when 4
+          if actual != 4
+            orari << giorno
+            giorno = []
+            previous_value = nil
+            actual = 4
+          end
+
+          if previous_value.nil?
+            previous_value = value
+          else
+            if previous_value >= value
+              orari = nil
+              break
+            end
+            chop = key.dup
+            chop.chop!
+            if chop == "timeoffuno" || chop == "timeoffdue" ||chop == "timeofftre"
+              giorno << previous_value
+              giorno << value
+            end
+            previous_value = nil
+          end
+        when 5
+          if actual != 5
+            orari << giorno
+            giorno = []
+            previous_value = nil
+            actual = 5
+          end
+
+          if previous_value.nil?
+            previous_value = value
+          else
+            if previous_value >= value
+              orari = nil
+              break
+            end
+            chop = key.dup
+            chop.chop!
+            if chop == "timeoffuno" || chop == "timeoffdue" ||chop == "timeofftre"
+              giorno << previous_value
+              giorno << value
+            end
+            previous_value = nil
+          end
+        when 6
+          if actual != 6
+            orari << giorno
+            giorno = []
+            previous_value = nil
+            actual = 6
+          end
+
+          if previous_value.nil?
+            previous_value = value
+          else
+            if previous_value >= value
+              orari = nil
+              break
+            end
+            chop = key.dup
+            chop.chop!
+            if chop == "timeoffuno" || chop == "timeoffdue" ||chop == "timeofftre"
+              giorno << previous_value
+              giorno << value
+            end
+            previous_value = nil
+          end
+        else
+        end
+      end
+    end
+    unless orari.nil?
+      orari << giorno
+    end
+    puts "orari #{orari}"
+    return orari
+
+  end
+
 end
